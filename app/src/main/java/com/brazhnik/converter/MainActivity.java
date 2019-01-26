06224279911cfc9d024dc0db2fae7b3026b8b19e
BrazhnikYuri <brazhnik.tts@gmail.com>
2019-01-27 05:59:38 +1100

    Version 0.1 -Added the ability to transfer rubles to dollars. * -Добавлена возможность переводить рубли в доллары.

--- app/src/main/java/com/brazhnik/converter/MainActivity.java
+++ app/src/main/java/com/brazhnik/converter/MainActivity.java
@@ -2,12 +2,32 @@ package com.brazhnik.converter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText et_money;
+    private Button btn_converte;
+    private TextView tv_result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_money = (EditText) findViewById(R.id.et_money);
+        btn_converte = (Button) findViewById(R.id.btn_converte);
+        tv_result = (TextView) findViewById(R.id.tv_result);
+        btn_converte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double rubles, dollars;
+                String rublesText = et_money.getText().toString();
+                rubles = Double.parseDouble(rublesText);
+                dollars = rubles/60;
+                String result = Double.toString(dollars);
+                tv_result.setText(result);
+            }
+        });
     }
 }
