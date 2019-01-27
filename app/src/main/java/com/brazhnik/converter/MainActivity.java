9e7b392bcecc9b289a8eb1314c29bbf9459be5b8
BrazhnikYuri <brazhnik.tts@gmail.com>
2019-01-27 21:51:26 +1100

    -Made some changes to improve the code. * -Сделаны небольшие изменения по улучшению кода.

--- app/src/main/java/com/brazhnik/converter/MainActivity.java
+++ app/src/main/java/com/brazhnik/converter/MainActivity.java
@@ -1,5 +1,4 @@
 package com.brazhnik.converter;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,25 +7,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText et_money;
-    private Button btn_converte;
-    private TextView tv_result;
+    private EditText etMoney;
+    private Button btnConvert;
+    private TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        et_money = (EditText) findViewById(R.id.et_money);
-        btn_converte = (Button) findViewById(R.id.btn_converte);
-        tv_result = (TextView) findViewById(R.id.tv_result);
-        btn_converte.setOnClickListener(new View.OnClickListener() {
+        etMoney = (EditText) findViewById(R.id.et_money);
+        btnConvert = (Button) findViewById(R.id.btn_convert);
+        tvResult = (TextView) findViewById(R.id.tv_result);
+        btnConvert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 double rubles, dollars;
-                String rublesText = et_money.getText().toString();
+                String rublesText = etMoney.getText().toString();
                 rubles = Double.parseDouble(rublesText);
                 dollars = rubles/60;
-                String result = Double.toString(dollars);
-                tv_result.setText(result);
+                String dollarsText = Double.toString(dollars);
+                String result = getString(R.string.result, dollarsText);
+                tvResult.setText(result);
             }
         });
     }
